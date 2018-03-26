a07440cc0d3eeb96e4924286841947df40ab5fbe
kizzonia <chibuikefortune@hotmail.com>
2018-03-26 16:14:21 +0200

    changes

--- app/mailers/reply_mailer.rb
+++ app/mailers/reply_mailer.rb
@@ -1,7 +1,8 @@
 class ReplyMailer < ApplicationMailer
   default :from => "support@zonia.co"
-  def reply_email(contact)
+  def reply_email(contact, mail)
     @contact = contact
+    @mail = mail
      mail(to: contact.Email, subject: 'Hello, Here is our solution to your request')
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
