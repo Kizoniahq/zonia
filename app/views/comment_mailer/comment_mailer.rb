29ba0749aba2cc99969eb2308f5a95e40269620f
kizzonia <chibuikefortune@hotmail.com>
2018-03-30 16:42:04 +0200

    added comments tests

--- app/views/comment_mailer/comment_mailer.rb
+++
@@ -1,9 +0,0 @@
-class CommentMailer < ApplicationMailer
-  default :from => "support@zonia.co"
-  def comment_email(contact, comment)
-    @contact = contact
-    @comment = comment
-     mail(to: @contact.email, subject: 'Hello, Here is our solution to your request')
-
-end
-end
